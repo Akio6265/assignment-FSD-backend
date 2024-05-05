7c574ae750af77cd37a3ab1f8a236cd99e450227
Aki <96163662+Akio6265@users.noreply.github.com>
2024-05-05 19:09:30 +0530

    fixing cors attempt_1

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,9 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 
 app.use("/signup", signup);
 app.use("/signout", signout);
