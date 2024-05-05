d152d671fb6b1a5ed58e5154f9c10553e576d38f
Aki <96163662+Akio6265@users.noreply.github.com>
2024-05-06 00:25:07 +0530

    admin update

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 const expressServer = app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running`);
 });
 
 const io = new Server(expressServer, {
--- src/routes/post/login.ts
+++ src/routes/post/login.ts
@@ -18,19 +18,21 @@ login.post(
     const {data: { username, password,code }} = req.body;
     const user = await getUserByUsername(username);
     if (!user) return res.status(400).json({ message: "User not found" });
+          const passwordVerification = passwordVerifier(password, user);
+          if (!passwordVerification) {
+            return res.status(409).json({ message: "Invalid password" });
+          }
+    if(!user.admin) {
     if(!code) return res.status(400).json({ message: "Provide 2FA code" });
-    const passwordVerification = passwordVerifier(password, user);
-    if (!passwordVerification) {
-      return res.status(409).json({ message: "Invalid password" });
+      const twoFactorAuth_secret = user?.twoFactorAuth_secret;
+      if (!twoFactorAuth_secret) {
+        return res.status(401).json({message:"No 2fa secret"});
+      }
+      const verified = authenticator.check(code, twoFactorAuth_secret);
+      if (!verified){
+        return res.status(401).json({ message: "Invalid code" });
+      }
     }
-    const twoFactorAuth_secret = user?.twoFactorAuth_secret;
-            if (!twoFactorAuth_secret) {
-              return res.status(401).json({message:"No 2fa secret"});
-            }
-            const verified = authenticator.check(code, twoFactorAuth_secret);
-            if (!verified){
-              return res.status(401).json({ message: "Invalid code" });
-}
     const tokens = tokenGenerator({
       name: user.username,
       id: user.userId,
