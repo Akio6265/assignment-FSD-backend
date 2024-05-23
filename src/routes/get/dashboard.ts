9c8c330f858d86246367563d061b79e9297ddd9b
Aki <96163662+Akio6265@users.noreply.github.com>
2024-05-23 14:59:33 +0530

    admin identity is hidden now

--- src/routes/get/dashboard.ts
+++ src/routes/get/dashboard.ts
@@ -1,7 +1,9 @@
 import { Router, Response } from "express";
 import { getAllLogsWithUsers, getUserLogs } from "../../../prisma/modules/get";
 import authorization from "../../middlewares/autorization";
+import dotenv from "dotenv";
 
+dotenv.config();
 const dashboard = Router();
 interface User {
     username: string;
@@ -16,26 +18,26 @@ dashboard.get("/", authorization, async (req: any, res: Response) => {
     userData: { id, name },
   } = req;
   //For admin
-  if (name === "SHIVAM") {
+  if (name === process.env.ADMIN) {
     const users_data = await getAllLogsWithUsers();
     if (!users_data) return res.json({ message: "No logs found" });
-     const logsWithUsername: Log[] = [];
-    users_data.forEach((user:User) => {
-      user.logs.forEach((log:Log) => {
-         const logWithUsername: Log & { username: string } = {
-           ...log,
-           username: user.username,
-         };
+    const logsWithUsername: Log[] = [];
+    users_data.forEach((user: User) => {
+      user.logs.forEach((log: Log) => {
+        const logWithUsername: Log & { username: string } = {
+          ...log,
+          username: user.username,
+        };
         logsWithUsername.push(logWithUsername);
       });
     });
-    res.json({ admin: true, admin_data: logsWithUsername, userLogs:false});
+    res.json({ admin: true, admin_data: logsWithUsername, userLogs: false });
     return;
   } else {
     //for regular
     const logs = await getUserLogs(id);
     if (!logs) return res.json({ message: "No logs found" });
-    res.json({ userLogs: logs,name });
+    res.json({ userLogs: logs, name });
   }
 });
 
